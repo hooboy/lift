fd0a945e8378c09f6fd1d245a9e9666fc0bfef23
hwb胡文博(深圳) <wb_hu@ctrip.com>
2018-03-24 20:20:03 +0800

    update 2018.3.24

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function component() {
     return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
 
 if (module.hot) {
     module.hot.accept('./print.js', function() {
